9fe1e63888675100bfe2bd4ce56f69b4ec3f7bfe
Shreyanekha31 <sr6256@srmist.edu.in>
2026-02-18 21:56:11 +0530

    UC2: Display OOPS banner using ASCII art

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,6 +1,12 @@
 public class HelloWorld {
-    public static void main(String args[]) {
-        System.out.println("Welcome to Classroom");
+    public static void main(String[] args) {
+
+        System.out.println(" *****   *****   *****    ***** ");
+        System.out.println("*     * *     * *     *  *     *");
+        System.out.println("*     * *     * *     *  *      ");
+        System.out.println("*     * *     * *****    ***** ");
+        System.out.println("*     * *     * *             *");
+        System.out.println("*     * *     * *       *     *");
+        System.out.println(" *****   *****   *        ***** ");
     }
 }
-
